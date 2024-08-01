376fd664facd7081334782de1859d0a590277fef
Anupa Ranabhat <anupa.ranabhat@gmail.com>
2024-08-01 10:48:35 +0545

    final changes

--- Game.cpp
+++ Game.cpp
@@ -4,7 +4,8 @@
 #include "Windows.h"
 #include "Pipe.h"
 #include "Game.h"
-#include <sstream>
+#include<sstream>
+
 
 Game::Game(sf::RenderWindow &window) : win(window), is_enter_pressed(false), is_mute_pressed(false),
                                        run_game(true),
@@ -12,19 +13,16 @@ Game::Game(sf::RenderWindow &window) : win(window), is_enter_pressed(false), is_
                                        pipe_counter(71),
                                        pipe_spawn_time(70),
                                        score(0)
-
+                                 
 {
     win.setFramerateLimit(60);
-    if (!bg_texture.loadFromFile("assets/bg.png"))
-    {
+     if (!bg_texture.loadFromFile("assets/bg.png")) {
         std::cerr << "Error: Could not load bg.png" << std::endl;
     }
-    if (!bg_texturea.loadFromFile("assets/bg_night.png"))
-    {
+    if (!bg_textureb.loadFromFile("assets/bg_night.jpg")) {
         std::cerr << "Error: Could not load bg_night.png" << std::endl;
     }
-    if (!bg_textureb.loadFromFile("assets/home.jpg"))
-    {
+      if (!bg_texturea.loadFromFile("assets/home.jpg")) {
         std::cerr << "Error: Could not load home.png" << std::endl;
     }
     bg_sprite.setTexture(bg_texture);
@@ -33,21 +31,13 @@ Game::Game(sf::RenderWindow &window) : win(window), is_enter_pressed(false), is_
     bg_sprite.setScale(SCALE_FACTOR, SCALE_FACTOR);
     bg_spritea.setScale(SCALE_FACTOR, SCALE_FACTOR);
     bg_spriteb.setScale(SCALE_FACTOR, SCALE_FACTOR);
-
+    
+   
     bg_sprite.setPosition(0.f, 0.f);
     bg_spritea.setPosition(bg_sprite.getGlobalBounds().width, 0.f);
     bg_spriteb.setPosition(bg_spritea.getGlobalBounds().width, 0.f);
 
-    if (!loading_texture.loadFromFile("assets/loading.gif"))
-    {
-        std::cerr << "Error: Could not load loading.gif" << std::endl;
-    }
-
-    loading_sprite.setTexture(loading_texture);
-    loading_sprite.setPosition(180, 280);
-
-    if (!volume_on_texture.loadFromFile("assets/volume_on.png"))
-    {
+    if (!volume_on_texture.loadFromFile("assets/volume_on.png")) {
         std::cerr << "Error: Could not load volume_on.png" << std::endl;
     }
     volume_on_sprite.setTexture(volume_on_texture);
@@ -67,36 +57,30 @@ Game::Game(sf::RenderWindow &window) : win(window), is_enter_pressed(false), is_
     ground_sprite1.setPosition(0.f, 575);
     ground_sprite2.setPosition(ground_sprite1.getGlobalBounds().width, 575);
 
-    font.loadFromFile("assets/pixel.ttf");
+    font.loadFromFile("assets/HWYGOTH.TTF");
     start_text.setFont(font);
-    start_text.setCharacterSize(30);
+    start_text.setCharacterSize(50);
     start_text.setFillColor(sf::Color::White);
     start_text.setPosition(180, 280);
     start_text.setString("Start Game");
 
     restart_text.setFont(font);
-    restart_text.setCharacterSize(30);
+    restart_text.setCharacterSize(50);
     restart_text.setFillColor(sf::Color::White);
-    restart_text.setPosition(200, 200);
-    restart_text.setString("Play Again");
+    restart_text.setPosition(180, 200);
+    restart_text.setString("Play Again!");
 
     scoreboard_text.setFont(font);
-    scoreboard_text.setCharacterSize(30);
+    scoreboard_text.setCharacterSize(50);
     scoreboard_text.setFillColor(sf::Color::White);
-    scoreboard_text.setPosition(220, 240);
+    scoreboard_text.setPosition(180, 250);
     scoreboard_text.setString("Score:");
 
     score_hud_text.setFont(font);
-    score_hud_text.setCharacterSize(20);
+    score_hud_text.setCharacterSize(34);
     score_hud_text.setFillColor(sf::Color::White);
     score_hud_text.setPosition(15, 15);
     score_hud_text.setString("Score: 0");
-
-    exit_text.setFont(font);
-    exit_text.setCharacterSize(25);
-    exit_text.setFillColor(sf::Color::White);
-    exit_text.setPosition(500, 700);
-    exit_text.setString("Exit");
 
     score_buffer.loadFromFile("assets/sfx/flap.wav");
     score_sound.setBuffer(score_buffer);
@@ -122,7 +106,7 @@ void Game::doProcessing(sf::Time &dt)
 
         for (auto it = pipes.begin(); it != pipes.end();)
         {
-            it->update(dt, score);
+            it->update(dt,score);
             if (it->getRightBound() < 0)
             {
                 it = pipes.erase(it);
@@ -151,13 +135,6 @@ void Game::startGameLoop()
             if (event.type == sf::Event::Closed)
             {
                 win.close();
-            }
-            else if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
-            {
-                if (exit_text.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
-                {
-                    win.close();
-                }
             }
             if (event.type == sf::Event::KeyPressed && run_game)
             {
@@ -259,18 +236,18 @@ void Game::checkScore()
 
 void Game::draw()
 {
-    if (score >= 2 && score <= 4)
+    if(score >=2 && score <=4)
     {
         bg_sprite.setTexture(bg_texturea);
         win.draw(bg_sprite);
-    }
-    else if (score > 4)
+    } 
+    else if(score >4)
     {
         bg_sprite.setTexture(bg_textureb);
         win.draw(bg_sprite);
+        
     }
-    else
-    {
+    else {
         bg_sprite.setTexture(bg_texture);
         win.draw(bg_sprite);
     }
@@ -290,7 +267,6 @@ void Game::draw()
     win.draw(start_text);
     win.draw(ground_sprite1);
     win.draw(ground_sprite2);
-    win.draw(exit_text);
     win.draw(bird.bird_sprite);
     win.draw(score_hud_text);
 
@@ -309,16 +285,19 @@ void Game::draw()
 void Game::moveGround(sf::Time &dt)
 {
     ground_sprite1.move(-move_speed * dt.asSeconds(), 0.f);
-    ground_sprite2.move(-move_speed * dt.asSeconds(), 0.f);
+	ground_sprite2.move(-move_speed * dt.asSeconds(), 0.f);
     if (ground_sprite1.getGlobalBounds().left + ground_sprite1.getGlobalBounds().width < 0)
-    {
-        ground_sprite1.setPosition(ground_sprite2.getGlobalBounds().left + ground_sprite2.getGlobalBounds().width, 578);
-    }
-    if (ground_sprite2.getGlobalBounds().left + ground_sprite2.getGlobalBounds().width < 0)
-    {
-        ground_sprite2.setPosition(ground_sprite1.getGlobalBounds().left + ground_sprite1.getGlobalBounds().width, 578);
-    }
+	{
+		ground_sprite1.setPosition(ground_sprite2.getGlobalBounds().left + ground_sprite2.getGlobalBounds().width, 578);
+	}
+	if (ground_sprite2.getGlobalBounds().left + ground_sprite2.getGlobalBounds().width < 0)
+	{
+		ground_sprite2.setPosition(ground_sprite1.getGlobalBounds().left + ground_sprite1.getGlobalBounds().width, 578);
+	}
+   
 }
+
+
 
 void Game::restartGame()
 {
@@ -338,4 +317,3 @@ std::string Game::toString(int num)
     ss << num;
     return ss.str();
 }
-// game
--- makefile
+++ makefile
@@ -2,6 +2,6 @@ all:compile link
 compile:
 	g++ -c main.cpp -c Game.cpp -c Bird.cpp -c Pipe.cpp -I"E:\OOP Lab\Project\Flappy-Bird\FIrst_SFML_Project-master\SFML-2.6.1\include" -DSFML_STATIC
 link:
-	g++ main.o Game.o Bird.o Pipe.o -o main -L"E:\OOP Lab\Project\Flappy-Bird\FIrst_SFML_Project-master\SFML-2.6.1\lib" -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-system
+	g++ main.o Game.o Bird.o Pipe.o -o main -L"E:\OOP Lab\Project\Flappy-Bird\FIrst_SFML_Project-master\SFML-2.6.1\lib" -lsfml-graphics -s -lsfml-window -s -lsfml-audio -s -lsfml-system -s -lopengl32 -lfreetype -lwinmm -lgdi32 -mwindows -lopenal32 -lflac -lvorbisenc -lvorbisfile -lvorbis -logg
 clean:
-	del -f *.o main game
+	del -f *.o main game -f *.exe main
